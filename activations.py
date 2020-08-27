Fixed Softmax overflow problem

activations.py
@@ -20,8 +20,12 @@ class Sigmoid(Activation):
 
 class Softmax(Activation):
     def call(self, x):
-        exp_x = np.exp(x)
-        return exp_x / np.sum(exp_x, axis=1, keepdims=True)
+        # note that softmax(x + c) = softmax(x) for any constant c
+        # we use this trick to avoid overflow
+
+        z = x - np.max(x, axis=-1, keepdims=True)
+        exp_z = np.exp(z) 
+        return exp_z / exp_z.sum(axis=-1, keepdims=True)
     
     def derivative(self, x):
         a = self.call(x)
@@ -45,6 +49,7 @@ class ReLU(Activation):
 
 ACTIVATIONS = {
     'sigmoid': Sigmoid,
+    'softmax': Softmax,
     'tanh': Tanh,
-    'relu': ReLU,
+    'relu': ReLU
 }

losses.py
@@ -39,7 +39,11 @@ class BinaryCrossentropy(Loss):
 class CategoricalCrossentropy(Loss):
     def call(self, y_true, y_pred):
         m = y_true.shape[0]
-        return -np.sum(y_true * np.log(y_pred)) / m
+
+        try:
+            return -np.sum(y_true * np.log(y_pred)) / m
+        except RuntimeWarning:
+            print(y_pred)
     
     def derivative(self, y_true, y_pred):
         """
@@ -47,7 +51,10 @@ class CategoricalCrossentropy(Loss):
         """
 
         m = y_true.shape[0]
-        return -(y_true / y_pred) / m
+        try:
+            return -(y_true / y_pred) / m
+        except RuntimeWarning:
+            print(y_pred)
 
 
 LOSSES = {

metrics.py
@@ -18,7 +18,21 @@ class BinaryAccuracy(Metric):
         return np.mean((y_true == y_pred).astype(np.int))
 
 
+class CategoricalAccuracy(Metric):
+    def __init__(self):
+        self.name = "accuracy"
+    
+    def call(self, y_true, y_pred):
+        y_pred = (y_pred >= 0.5).astype(np.int)
+        y_pred = np.argmax(y_pred, axis=1)
+        y_true = np.argmax(y_true, axis=1)
+        return np.mean((y_true == y_pred).astype(np.int))
+
+
 METRICS = {
-    "accuracy": BinaryAccuracy,
-    "acc": BinaryAccuracy
+    "binary_accuracy": BinaryAccuracy,
+    "categorical_accuracy": CategoricalAccuracy,
+    # "acc": BinaryAccuracy
 }
+
+print(METRICS)